78950edf51e73304aaecf8fb1ccccbaa6fda3ab0
ninh.do <ninh.do@blueoc.tech>
2025-06-11 23:48:36 +0700

    fix: fix payment

--- src/components/body/QuantitySelector.jsx
+++ src/components/body/QuantitySelector.jsx
@@ -1,44 +1,9 @@
-import { Button, InputGroup, FormControl } from "react-bootstrap";
-import React, { useState } from "react";
 
 const QuantitySelector = ({ price, sale, discount }) => {
-  const [quantity, setQuantity] = useState(1);
-  const handlePrev = () => {
-    setQuantity((prev) => {
-      return prev + 1;
-    });
-  };
-  const handleNext = () => {
-    setQuantity((next) => {
-      if (quantity < 2) {
-        alert("So luong toi thieu can mua la 1");
-        setQuantity(1);
-      }
-      return next - 1;
-    });
-  };
-  const handleValue = (e) => {
-    setQuantity(e.target.value);
-  };
+
 
   return (
     <div className="d-flex mx-5" style={{ minWidth: "250px" }}>
-      <InputGroup
-        style={{ maxWidth: "115px", maxHeight: "1vh", marginRight: "20px" }}
-      >
-        <Button variant="light" onClick={handleNext}>
-          -
-        </Button>
-        <FormControl
-          type="number"
-          style={{ textAlign: "center" }}
-          value={quantity}
-          onChange={handleValue}
-        />
-        <Button variant="light" onClick={handlePrev}>
-          +
-        </Button>
-      </InputGroup>
       <div>
         <div>
           <p className="m-0 fw-bold">
